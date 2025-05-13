081c2e81aedacf0a1024eafe47508bc669e64728
Omar Soriano <osoriano2@gmail.com>
2025-05-13 11:16:40 +0000

    Update argocd step variables
    Break the variables into the different components. Also,
    remove unused variables

--- argocd/deploy-step-argocd.sh
+++ argocd/deploy-step-argocd.sh
@@ -41,11 +41,17 @@ KEY_PATH="${6}"
 # Path to the resource inside the repo. If a directory, runs all on files in directory
 RESOURCE_PATH="${7}"
 
-# New deployment image to substute in the specified resources
-NEW_IMAGE="${8}"
+# The registry of the image. Will be used as a prefix of the full image name
+IMAGE_REGISTRY="${8}"
 
-# Whether the deploy step is approved. This should be set to 'YES'. Otherwise, the step exits with code 1
-DEPLOYMENT_APPROVED="${9}"
+# The image repository prefix
+IMAGE_REPO_PREFIX="${9}"
+
+# The image tag
+IMAGE_TAG="${10}"
+
+# The image repository suffix
+IMAGE_REPO_SUFFIX="${11}"
 
 echo "Deploying with parameters:"
 echo "  REPO_URL=${REPO_URL}"
@@ -55,14 +61,14 @@ echo "  APP_USER_ID=${APP_USER_ID}"
 echo "  APP_USER_NAME=${APP_USER_NAME}"
 echo "  KEY_PATH=${KEY_PATH}"
 echo "  RESOURCE_PATH=${RESOURCE_PATH}"
-echo "  NEW_IMAGE=${NEW_IMAGE}"
-echo "  DEPLOYMENT_APPROVED=${DEPLOYMENT_APPROVED}"
+echo "  IMAGE_REGISTRY=${IMAGE_REGISTRY}"
+echo "  IMAGE_REPO_PREFIX=${IMAGE_REPO_PREFIX}"
+echo "  IMAGE_TAG=${IMAGE_TAG}"
+echo "  IMAGE_REPO_SUFFIX=${IMAGE_REPO_SUFFIX}"
 
-# Exit with error if the deployment is not approved
-if [[ "${DEPLOYMENT_APPROVED}" != YES ]]; then
-  echo "Fail the deployment due to approval status: ${DEPLOYMENT_APPROVED}"
-  exit 1
-fi
+FULL_IMAGE_NAME="${IMAGE_REGISTRY}${IMAGE_REPO_PREFIX}${IMAGE_REPO_SUFFIX}:${IMAGE_TAG}"
+echo "Derived parameters:"
+echo "  FULL_IMAGE_NAME=${FULL_IMAGE_NAME}"
 
 # Clone the repo
 echo "Cloning the repo"
@@ -73,21 +79,19 @@ cd /repo
 echo "Configuring git"
 git config user.name "${APP_USER_NAME}"
 git config user.email "${APP_USER_ID}+${APP_USER_NAME}@users.noreply.github.com"
-GH_ACCESS_TOKEN="$(generate-installation-access-token "${APP_ID}" "${KEY_PATH}" "${NEW_IMAGE}")"
+GH_ACCESS_TOKEN="$(generate-installation-access-token "${APP_ID}" "${KEY_PATH}" "${IMAGE_REPO_PREFIX}")"
 git config user.password "${GH_ACCESS_TOKEN}"
 
 # Perform the subtitution
 echo "Substituting image version"
-IMAGE_WITHOUT_TAG="${NEW_IMAGE%%:*}"
-NEW_TAG="${NEW_IMAGE##*:}"
 if [[ -d "${RESOURCE_PATH}" ]]; then
   for resource_path_part in "${RESOURCE_PATH}"/*; do
     if [[ -f "${resource_path_part}" ]]; then
       file_ext=${resource_path_part##*.}
       if [[ "${file_ext}" == yaml || "${file_ext}" == yml || "${file_ext}" == json ]]; then
         echo "Substituting image version for: ${resource_path_part}"
-        sed --regexp-extended "s|${IMAGE_WITHOUT_TAG}:[a-zA-Z0-9_.-]+|${NEW_IMAGE}|g" -i "${resource_path_part}"
-        sed --regexp-extended "s|(app\\.kubernetes\\.io/version[\": ]+)[a-zA-Z0-9_.-]+|\\1${NEW_TAG}|g" -i "${resource_path_part}"
+        sed --regexp-extended "s|${IMAGE_REPO_PREFIX}${IMAGE_REPO_SUFFIX}:[a-zA-Z0-9_.-]+|${FULL_IMAGE_NAME}|g" -i "${resource_path_part}"
+        sed --regexp-extended "s|(app\\.kubernetes\\.io/version[\": ]+)[a-zA-Z0-9_.-]+|\\1${IMAGE_TAG}|g" -i "${resource_path_part}"
       else
         echo "Warning: unknown file extension ${file_ext}"
       fi
@@ -98,8 +102,8 @@ if [[ -d "${RESOURCE_PATH}" ]]; then
   done
 elif [[ -f "${RESOURCE_PATH}" ]]; then
   echo "Substituting image version for: ${RESOURCE_PATH}"
-  sed --regexp-extended "s|${IMAGE_WITHOUT_TAG}:[a-zA-Z0-9_.-]+|${NEW_IMAGE}|g" -i "${RESOURCE_PATH}"
-  sed --regexp-extended "s|(app\\.kubernetes\\.io/version[\": ]+)[a-zA-Z0-9_.-]+|\\1${NEW_TAG}|g" -i "${RESOURCE_PATH}"
+  sed --regexp-extended "s|${IMAGE_REPO_PREFIX}${IMAGE_REPO_SUFFIX}:[a-zA-Z0-9_.-]+|${FULL_IMAGE_NAME}|g" -i "${RESOURCE_PATH}"
+  sed --regexp-extended "s|(app\\.kubernetes\\.io/version[\": ]+)[a-zA-Z0-9_.-]+|\\1${IMAGE_TAG}|g" -i "${RESOURCE_PATH}"
 else
   echo "Resource path does not exist: ${RESOURCE_PATH}"
   exit 1
@@ -112,10 +116,10 @@ if git diff --quiet; then
   echo "Exiting early"
   exit 0
 fi
-git commit -am "Bump ${RESOURCE_PATH} to \`${NEW_TAG:0:8}\`
+git commit -am "Bump ${RESOURCE_PATH} to \`${IMAGE_TAG:0:8}\`
 
 Bump resource ${RESOURCE_PATH} to version:
-${NEW_TAG}"
+${IMAGE_TAG}"
 
 NEW_REPO_URL="${REPO_URL/github.com/${APP_USER_NAME}:${GH_ACCESS_TOKEN}@github.com}"
 git remote set-url origin "${NEW_REPO_URL}"
@@ -129,7 +133,7 @@ if [[ -d "${RESOURCE_PATH}" ]]; then
       file_ext=${resource_path_part##*.}
       if [[ "${file_ext}" == yaml || "${file_ext}" == yml || "${file_ext}" == json ]]; then
         echo "Waiting for resource: ${resource_path_part}"
-        wait-for-resource "${resource_path_part}" "${NEW_TAG}"
+        wait-for-resource "${resource_path_part}" "${IMAGE_TAG}"
       else
         echo "Warning: unknown file extension ${file_ext}"
       fi
@@ -140,7 +144,7 @@ if [[ -d "${RESOURCE_PATH}" ]]; then
   done
 elif [[ -f "${RESOURCE_PATH}" ]]; then
   echo "Waiting for resource: ${RESOURCE_PATH}"
-  wait-for-resource "${RESOURCE_PATH}" "${NEW_TAG}"
+  wait-for-resource "${RESOURCE_PATH}" "${IMAGE_TAG}"
 else
   echo "Resource path does not exist: ${RESOURCE_PATH}"
   exit 1
